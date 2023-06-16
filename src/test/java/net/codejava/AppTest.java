e51eec779bf11323b2bc44f38de015df40e86450
dario.guardia <dario.guardia@ip-192-168-0-101.sa-east-1.compute.internal>
2023-06-16 16:17:46 -0300

    test

--- src/test/java/net/codejava/AppTest.java
+++ src/test/java/net/codejava/AppTest.java
@@ -1,20 +1,25 @@
 package net.codejava;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAppConstructor() {
+        App app1 = new App();
+        App app2 = new App();
+        assertEquals(app1.getMessage(), app2.getMessage());
+        System.out.println("Passed 1");
+    }
+
+    @Test
+    public void testAppMessage() {
+        App app = new App();
+        assertEquals("Hello World!", app.getMessage());
     }
 }
